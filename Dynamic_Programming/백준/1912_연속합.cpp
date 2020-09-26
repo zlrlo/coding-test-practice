c7fe860e55e3f8782c7ec0e484e0008aea2eb08b
zlrlo <easy10612@gmail.com>
2020-09-26 23:09:20 +0900

    연속합

---
+++ Dynamic_Programming/백준/1912_연속합.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std; 
+
+int arr[100010];
+
+int main() {
+	int n; 
+	cin >> n; 
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
+	}
+
+	int maxSum = -1000; 
+	int sum = 0; 
+	for (int i = 0; i < n; i++) {
+		if (sum < 0) sum = arr[i]; 
+		else sum += arr[i]; 
+
+		if (sum > maxSum) maxSum = sum; 
+	}
+
+	cout << maxSum; 
+	return 0; 
+}
