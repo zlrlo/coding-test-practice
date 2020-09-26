8a7db8e82d516276a912651395f999a64f20f61f
zlrlo <easy10612@gmail.com>
2020-09-26 19:59:39 +0900

    123 더하기

---
+++ Dynamic_Programming/백준/9095_123_더하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main() {
+	int t; 
+	cin >> t; 
+
+	while (t--) {
+		int n; 
+		cin >> n;
+
+		int dp[20] = { 0 };
+
+		dp[3] = 1; dp[4] = 1; dp[5] = 1; 
+
+		for (int i = 3; i <= n + 2; i++) {
+			dp[i] += dp[i - 1] + dp[i - 2] + dp[i - 3]; 
+		}
+
+		cout << dp[n + 2] << endl; 
+	}
+
+	return 0; 
+}
