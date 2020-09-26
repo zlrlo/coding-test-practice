7087218ed65f10f4c05459e65faeaf6ad13c48a2
zlrlo <easy10612@gmail.com>
2020-09-26 19:56:57 +0900

    가장 긴 증가하는 부분 수열

---
+++ Dynamic_Programming/백준/11053_가장_긴_증가하는_부분_수열.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int main() {
+	int n;
+	cin >> n; 
+
+	int num[1100]; 
+	for (int i = 0; i < n; i++) {
+		cin >> num[i]; 
+	}
+
+	int dp[1100] = { 0 };
+	dp[0] = 1;
+	int maxValue = 0; 
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1; 
+		for (int j = 0; j < i; j++) {
+			if (num[j] >= num[i]) continue; 
+			dp[i] = max(dp[i], dp[j] + 1); 
+		}
+		if (dp[i] > maxValue) maxValue = dp[i]; 
+ 	}
+
+	cout << maxValue; 
+	return 0; 
+}
