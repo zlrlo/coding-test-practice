5b0765e20bfc387b6186aa695e6c78590721df91
zlrlo <easy10612@gmail.com>
2020-09-26 19:59:27 +0900

    이친수

---
+++ Dynamic_Programming/백준/2193_이친수.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std; 
+
+long long dp[100][2]; 
+
+int main() {
+	int n; 
+	cin >> n; 
+
+	dp[1][0] = 0; 
+	dp[1][1] = 1; 
+
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = dp[i - 1][1] + dp[i - 1][0]; 
+		dp[i][1] = dp[i - 1][0]; 
+	}
+
+	cout << dp[n][0] + dp[n][1]; 
+
+	return 0; 
+}
