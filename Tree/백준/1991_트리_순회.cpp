b310a319dbf04c1e4b13337128350a35f9d8d9d3
zlrlo <easy10612@gmail.com>
2020-10-22 14:36:30 +0900

    트리 중위 순회 문제 추가

---
+++ Tree/백준/1991_트리_순회.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+char tree[30][2];
+vector<char> preorder;
+vector<char> postorder;
+vector<char> inorder;
+
+void Order(int level) {
+	if (tree[level][0] >= 'A' && tree[level][0] <= 'Z') {
+		preorder.push_back(tree[level][0]);
+		Order(tree[level][0] - 'A');
+		postorder.push_back(tree[level][0]);
+	}
+
+	inorder.push_back(level + 'A');
+
+	if (tree[level][1] >= 'A' && tree[level][1] <= 'Z') {
+		preorder.push_back(tree[level][1]);
+		Order(tree[level][1] - 'A');
+		postorder.push_back(tree[level][1]);
+	}
+}
+
+void Print() {
+	for (auto i : preorder) {
+		cout << i;
+	}
+	cout << endl;
+	for (auto i : inorder) {
+		cout << i;
+	}
+	cout << endl;
+	for (auto i : postorder) {
+		cout << i;
+	}
+}
+
+int main() {
+	//freopen("text.txt", "r", stdin); 
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		char root, left, right;
+		cin >> root >> left >> right;
+
+		tree[root - 'A'][0] = left;
+		tree[root - 'A'][1] = right;
+	}
+
+	preorder.push_back('A');
+	Order(0);
+	postorder.push_back('A');
+
+	Print();
+
+	return 0;
+}
---
+++ Tree/백준/2250_트리의_높이와_너비.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+struct Node
+{
+	int minWidth;
+	int maxWidth;
+};
+
+struct Ans
+{
+	int name;
+	int value;
+};
+
+int tree[10010][2];
+int bucket[10010];
+map<int, Node> levelMap;
+
+int findRoot(int n) {
+	for (int i = 0; i < n; i++) {
+		if (bucket[i] == 1) {
+			return i;
+		}
+	}
+}
+
+int index = 1;
+
+void Order(int now, int level) {
+	if (now < 0) return;
+
+	Order(tree[now][0], level + 1);
+
+	if (levelMap.count(level)) {
+		levelMap[level].maxWidth = index++;
+	}
+	else {
+		levelMap[level].minWidth = index++;
+	}
+
+	Order(tree[now][1], level + 1);
+}
+
+int main() {
+	//freopen("text.txt", "r", stdin); 
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int root, left, right;
+		cin >> root >> left >> right;
+
+		bucket[root] += 1;
+		bucket[left] += 1;
+		bucket[right] += 1;
+
+		tree[root][0] = left;
+		tree[root][1] = right;
+	}
+
+	int root = findRoot(n);
+
+	Order(root, 1);
+
+	levelMap[root].maxWidth = levelMap[root].minWidth;
+
+	Ans answer = { -1, -1 };
+	for (auto i : levelMap) {
+		int value = i.second.maxWidth - i.second.minWidth + 1;
+		if (answer.name == -1) {
+			answer.name = i.first;
+			answer.value = value;
+		}
+		else {
+			if (answer.value < value) {
+				answer.name = i.first;
+				answer.value = value;
+			}
+		}
+	}
+
+	cout << answer.name << " " << answer.value;
+
+	return 0;
+}
