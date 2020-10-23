a323654a8babd4ee950d29489fb087971c379377
zlrlo <easy10612@gmail.com>
2020-10-23 15:45:24 +0900

    트리의 지름
    - 출처 : 백준
    - 유형 : 트리의 지름

---
+++ Tree/백준/1167_트리의_지름.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std; 
+
+struct Node
+{
+	int name; 
+	int dist; 
+};
+
+vector<Node> tree[100010]; 
+int n; 
+
+void Input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int root;
+		cin >> root;
+
+		int name, dist;
+		for (int j = 0; ; j++) {
+			cin >> name;
+			if (name < 0) break;
+			cin >> dist;
+			tree[root].push_back({ name, dist });
+		}
+	}
+	
+}
+
+bool via[100010]; 
+int maxValue; 
+int maxIndex; 
+
+void Run(int now, int sum) {
+	via[now] = true; 
+	if (sum > maxValue) {
+		maxValue = sum;
+		maxIndex = now; 
+	}
+
+	for (auto i : tree[now]) {
+		if (via[i.name]) continue; 
+		Run(i.name, sum + i.dist);
+	}
+
+
+}
+
+int main() {
+	freopen("Text.txt", "r", stdin); 
+
+	Input(); 
+
+	Run(1, 0);
+	memset(via, 0, n + 1); 
+	Run(maxIndex, 0); 
+	
+
+	cout << maxValue; 
+
+	return 0; 
+}
---
+++ Tree/백준/1967_트리의_지름.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std; 
+
+struct Node
+{
+	int name; 
+	int dist; 
+};
+
+int n; 
+vector<Node> tree[10010]; 
+bool exitFlag; 
+
+void Input() {
+	cin >> n; 
+	if (n == 1) {
+		exitFlag = true; 
+		return; 
+	}
+	
+	int parent, child, dist; 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> parent >> child >> dist; 
+			tree[parent].push_back({ child, dist }); 
+			tree[child].push_back({ parent, dist });
+		}
+	}
+}
+
+
+bool via[10010]; 
+int maxValue; 
+int maxIndex; 
+
+void Run(int now, int sum) {
+	via[now] = true; 
+
+	if (sum > maxValue) {
+		maxValue = sum; 
+		maxIndex = now; 
+	}
+
+	for (auto i : tree[now]) {
+		if (via[i.name]) continue; 
+		Run(i.name, sum + i.dist); 
+	}
+}
+
+int main() {
+	//freopen("Text.txt", "r", stdin); 
+
+	Input(); 
+
+	if (exitFlag) {
+		cout << 0; 
+		return 0; 
+	}
+
+	Run(1, 0); 
+
+	memset(via, 0, n + 1); 
+	Run(maxIndex, 0); 
+
+	cout << maxValue; 
+	return 0; 
+}
