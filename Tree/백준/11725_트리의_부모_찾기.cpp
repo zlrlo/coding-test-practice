fa4dee9c779e647f1784e1fc6e8788de528bb221
zlrlo <easy10612@gmail.com>
2020-10-23 15:45:03 +0900

    트리의 부모 찾기
    출처 : 백준
    유형 : 트리

---
+++ Tree/백준/11725_트리의_부모_찾기.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+
+const int MAX = 100010; 
+
+vector<int> graph[MAX];
+
+int parentInfo[MAX]; 
+bool via[MAX]; 
+
+void Run(int now) {
+	via[now] = true; 
+
+	for (auto i : graph[now]) {
+		if (via[i]) continue; 
+		parentInfo[i] = now; 
+		Run(i); 
+	}
+}
+
+int main() {
+	freopen("Text.txt", "r", stdin); 
+
+	int n; 
+	cin >> n; 
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b; 
+		cin >> a >> b; 
+
+		graph[a].push_back(b); 
+		graph[b].push_back(a); 
+	}
+
+	Run(1); 
+
+	for (int i = 2; i <= n; i++) {
+		cout << parentInfo[i] << "\n";
+	}
+
+	return 0; 
+}
