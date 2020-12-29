dae135f3f24bbff49ee2f37e82b601bedf92e5b6
zlrlo <easy10612@gmail.com>
2020-12-29 22:12:28 +0900

    [문제추가] 두개 뽑아서 더하기
    - assign 처음 써봄

---
+++ Programmers/Level1-2/두개_뽑아서_더하기.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+set<int> bucket; 
+
+int main() {
+//vector<int> solution(vector<int> numbers) {
+	vector<int> numbers = { 5, 0, 2, 7 };
+	vector<int> answer;
+
+	int len = numbers.size(); 
+
+	for (int i = 0; i < len; i++) {
+		for (int j = i + 1; j < len; j++) {
+			bucket.insert(numbers[i] + numbers[j]);
+		}
+	}
+
+	answer.assign(bucket.begin(), bucket.end()); 
+
+	//return answer;
+	return 0; 
+}
